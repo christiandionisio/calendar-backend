86224517c17b74b2d00e3e3fe372661b70ef0bd6
Christian Dionisio <christiandionisio9432@gmail.com>
2022-04-20 12:56:58 -0500

    crear middleware para validar campos

--- routes/auth.js
+++ routes/auth.js
@@ -7,13 +7,15 @@ const { Router } = require('express');
 const { check } = require('express-validator')
 const router = Router();
 
-const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth')
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 router.post('/new', 
     [
         check('name', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El email es obligatorio').isEmail(),
         check('password', 'El password debe de ser de 6 caracteres').isLength({min: 6}),
+        validarCampos
     ] , 
     crearUsuario
 );
@@ -22,6 +24,7 @@ router.post('/',
     [
         check('email', 'El email es obligatorio').isEmail(),
         check('password', 'El password debe de ser de 6 caracteres').isLength({min: 6}),
+        validarCampos
     ],
     loginUsuario
 );
