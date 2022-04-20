6a9f89cc01a20660db5aa4eca78e7ba0e7e6f97a
Christian Dionisio <christiandionisio9432@gmail.com>
2022-04-20 09:06:03 -0500

    controllers auth creado

---
+++ controllers/auth.js
@@ -0,0 +1,35 @@
+const { response } = require('express');
+
+const crearUsuario = (req, res = response) => {
+
+    res.json({
+        ok: true,
+        msg: 'registro'
+    });
+
+}
+
+const loginUsuario = (req, res = response) => {
+
+    res.json({
+        ok: true,
+        mosg: 'login'
+    });
+
+}
+
+const revalidarToken = (req, res = response) => {
+
+    res.json({
+        ok: true,
+        msg: 'renew'
+    });
+
+}
+
+
+module.exports = {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken
+}
