fe259f31fecca1a94a0783201b1c7e22b0935035
Christian Dionisio <christiandionisio9432@gmail.com>
2022-04-20 08:39:43 -0500

    express configurado

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+const express = require('express');
+
+// Crear el servidor de express
+const app = express();
+
+// Rutas
+app.get('/', (req, res) => {
+
+    console.log('se requiere /');
+    res.json({
+        ok: true
+    });
+
+});
 
 
-
-console.log('Hola mundo!!');
+// Escuuchar peticiones
+app.listen(4000, () => {
+    console.log(`Servidor corriendo en puerto ${4000}`);
+});
