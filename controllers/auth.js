3bd2c65b303d28220b067916d19db599ffd6782e
Christian Dionisio <christiandionisio9432@gmail.com>
2022-04-21 09:05:27 -0500

    registrar y validar usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,38 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario')
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
 
-    const {name, email, password} = req.body;
+    const {email, password} = req.body;
 
-    res.status(201).json({
-        ok: true,
-        msg: 'registro',
-        name,
-        email,
-        password
-    });
+    try {
+
+        let usuario = await Usuario.findOne({email});
+        
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Un usuario existe con ese correo'
+            })
+        }
+
+        usuario = new Usuario(req.body);
+    
+        await usuario.save();
+    
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        })
+    }
+
 
 }
 
